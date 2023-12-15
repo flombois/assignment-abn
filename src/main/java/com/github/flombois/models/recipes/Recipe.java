1e067512fcbd37cf4c2f4a3fb4ab0c18b21beeda
flombois <flombois@users.noreply.github.com>
2023-12-15 15:08:04 +0100

    Add many-to-many relationship between recipes and tags

--- src/main/java/com/github/flombois/models/recipes/Recipe.java
+++ src/main/java/com/github/flombois/models/recipes/Recipe.java
@@ -1,6 +1,7 @@
 package com.github.flombois.models.recipes;
 
 import com.github.flombois.models.BaseEntity;
+import com.github.flombois.models.tags.Tag;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 
@@ -39,4 +40,59 @@ public class Recipe extends BaseEntity {
             foreignKey = @ForeignKey(name = "FK_STEPS_ON_RECIPE"))
     @OrderColumn(name = "step_order")
     private List<@NotNull Step> steps;
+
+    @Size(max = 256)
+    @ManyToMany
+    @JoinTable(name = "recipes_tags",
+        joinColumns = @JoinColumn(name = "recipe_id"),
+        inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private List<@NotNull Tag> tags;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public List<Quantity> getQuantities() {
+        return quantities;
+    }
+
+    public void setQuantities(List<Quantity> quantities) {
+        this.quantities = quantities;
+    }
+
+    public List<Step> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<Step> steps) {
+        this.steps = steps;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
 }
--- src/main/java/com/github/flombois/models/tags/Tag.java
+++ src/main/java/com/github/flombois/models/tags/Tag.java
@@ -1,10 +1,15 @@
 package com.github.flombois.models.tags;
 
 import com.github.flombois.models.BaseEntity;
+import com.github.flombois.models.recipes.Recipe;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
+
+import java.util.List;
 
 @Entity(name = "tags")
 public class Tag extends BaseEntity {
@@ -13,4 +18,23 @@ public class Tag extends BaseEntity {
     @Length(max = 255)
     @Column(nullable = false, unique = true)
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<@NotNull Recipe> recipes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(List<Recipe> recipes) {
+        this.recipes = recipes;
+    }
 }
